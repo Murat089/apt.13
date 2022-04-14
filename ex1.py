b60a479bb9184b8a7524dcbf09881ee9c4bd3403
Murat089 <wassermann505@gmail.com>
2022-04-14 20:22:41 +0600

    init4

---
+++ ex1.py
@@ -0,0 +1,8 @@
+while True:
+    i = input('Input: ')
+    print(i)
+    if i in ('go', 'java', 'php', 'python', 'javascript', 'ruby'):
+        print('this languages is in list')
+        break
+
+
---
+++ ex3.py
@@ -0,0 +1,7 @@
+a = 7
+for i in range(5):
+    a *= a
+    print(a)
+
+for o in range(10):
+    print(o)
